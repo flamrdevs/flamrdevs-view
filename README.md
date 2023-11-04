42fdfe12d0db58fd668c26c5d2c95e5a2735b08c
flamrdevs <flamrdevs@gmail.com>
2023-11-04 14:30:45 +0700

    chore: initialize app

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# flamrdevs-view
-view tracker
+<div align="center">
+  <picture title="flamrdevs">
+    <source media="(prefers-color-scheme: dark)" srcset="https://flamrdevs.pages.dev/badge/dark.svg">
+    <img alt="flamrdevs" src="https://flamrdevs.pages.dev/badge/light.svg">
+  </picture>
+</div>
---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { logger } from "hono/logger";
+import { secureHeaders } from "hono/secure-headers";
+
+const app = new Hono()
+  .use("*", logger())
+  .use("*", cors({ origin: "*" }))
+
+  .get("/health", (ctx) => ctx.json({ ok: true }, 200))
+
+  .use("*", secureHeaders())
+
+  .get("/", (ctx) => ctx.json({ name: "api" }))
+  .notFound((ctx) => ctx.json({ message: "Not found" }, 404))
+  .onError((error: unknown, ctx) => {
+    let status = 500;
+    let message = "Internal server error";
+
+    if (error instanceof Error) {
+      message = error.message;
+    }
+
+    return ctx.json({ message }, status);
+  });
+
+export default app;
