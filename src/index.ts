15bbc504c6f73d5c8598b4aac36e8aaca3fe0765
flamrdevs <flamrdevs@gmail.com>
2023-12-20 23:50:07 +0700

    chore: improvements

--- src/index.ts
+++ src/index.ts
@@ -31,47 +31,44 @@ const app = new Hono<HonoEnv>()
   .get("/health", (ctx) => ctx.json({ ok: true }, 200))
   .use("*", secureHeaders({ crossOriginResourcePolicy: false }));
 
-(() => {
-  const profile = <T extends string>(name: T) => {
-    app.route(
-      `/${name}`,
-      (() => {
-        const KEY_TOTAL = `${name}/total`;
-        const KEY_TIMES = `${name}/times`;
-        return new Hono<HonoEnv>()
-          .get("/", async (ctx) => {
-            const redis = redisFromContext(ctx);
-            let stroke = "#6E6E6E";
-            try {
-              await Promise.all([redis.incr(KEY_TOTAL), redis.lpush<number>(KEY_TIMES, Date.now())]);
-            } catch (error) {
-              console.error(error);
-              stroke = "#E5484D";
-            }
-            return ctx.body(
-              `<svg xmlns="http://www.w3.org/2000/svg" height="1" width="100%"><line x1="0" y1="0" x2="100%" y2="0" stroke="${stroke}" stroke-width="1" /></svg>`,
-              200,
-              { "Content-Type": "image/svg+xml", "Cache-Control": "max-age=0, no-cache, no-store, must-revalidate" }
-            );
-          })
-          .get("/total", async (ctx) => ctx.json(Number(await redisFromContext(ctx).get<string>(KEY_TOTAL))))
-          .get("/times", async (ctx) => ctx.json(await redisFromContext(ctx).lrange<number>(KEY_TIMES, 0, -1)))
-          .put("/slice", async (ctx) => {
-            await secret(ctx);
-            return ctx.json({ ok: (await redisFromContext(ctx).ltrim(KEY_TIMES, 0, 999)) === "OK" });
-          })
-          .delete("/clear", async (ctx) => {
-            await secret(ctx);
-            const redis = redisFromContext(ctx);
-            return ctx.json({ ok: (await Promise.all([redis.del(KEY_TOTAL), redis.del(KEY_TIMES)])).every((deleted) => deleted > 0) });
-          });
-      })()
-    );
-  };
+const profile = <T extends string>(name: T) => {
+  app.route(
+    `/${name}`,
+    (() => {
+      const KEY_TOTAL = `${name}/total`;
+      const KEY_TIMES = `${name}/times`;
+      return new Hono<HonoEnv>()
+        .get("/", async (ctx) => {
+          const redis = redisFromContext(ctx);
+          let stroke = "#6E6E6E";
+          try {
+            await Promise.all([redis.incr(KEY_TOTAL), redis.lpush<number>(KEY_TIMES, Date.now())]);
+          } catch (error) {
+            console.error(error);
+            stroke = "#E5484D";
+          }
+          return ctx.body(
+            `<svg xmlns="http://www.w3.org/2000/svg" height="1" width="100%"><line x1="0" y1="0" x2="100%" y2="0" stroke="${stroke}" stroke-width="1" /></svg>`,
+            200,
+            { "Content-Type": "image/svg+xml", "Cache-Control": "max-age=0, no-cache, no-store, must-revalidate" }
+          );
+        })
+        .get("/total", async (ctx) => ctx.json(Number(await redisFromContext(ctx).get<string>(KEY_TOTAL))))
+        .get("/times", async (ctx) => ctx.json(await redisFromContext(ctx).lrange<number>(KEY_TIMES, 0, -1)))
+        .put("/slice", async (ctx) => {
+          await secret(ctx);
+          return ctx.json({ ok: (await redisFromContext(ctx).ltrim(KEY_TIMES, 0, 999)) === "OK" });
+        })
+        .delete("/clear", async (ctx) => {
+          await secret(ctx);
+          return ctx.json({ ok: (await redisFromContext(ctx).del(KEY_TOTAL, KEY_TIMES)) > 0 });
+        });
+    })()
+  );
+};
 
-  profile("github");
-  profile("gitlab");
-})();
+profile("github");
+profile("gitlab");
 
 app
   .get("/", (ctx) => ctx.json({ name: "view" }))
